98ccb87f29fab0eda5a2bbf06542bee4ada7ce79
Michael Yang <themichaelyang@gmail.com>
2017-04-18 16:25:32 -0400

    refactored code to use tutorial instead of tour

--- game/game.stages.js
+++ game/game.stages.js
@@ -13,18 +13,18 @@ module.exports = function(stager, settings) {
 
     stager
         .next('instructions')
-        .next('choose-tour') // choose blue/red
-        .repeat('tour', settings.tour.length) //
-        .next('tour-end') // remind players that they are randomly assigned to blue/red
+        .next('choose-tutorial') // choose blue/red
+        .repeat('tutorial', settings.tutorial.length) //
+        .next('tutorial-end') // remind players that they are randomly assigned to blue/red
         // .repeat('practice', settings.REPEAT_PRACTICE)
         // .next('practice-end')
         .gameover();
 
-    stager.extendStage('tour', {
+    stager.extendStage('tutorial', {
     	steps: [
-    	    'red-choice-tour',
-    	    'blue-choice-tour',
-          'results-tour'
+    	    'red-choice-tutorial',
+    	    'blue-choice-tutorial',
+          'results-tutorial'
     	]
     });
 
@@ -37,14 +37,14 @@ module.exports = function(stager, settings) {
     // });
 
     // stager.skip('instructions');
-    // stager.skip('choose-tour');
-    // stager.skip('tour');
+    // stager.skip('choose-tutorial');
+    // stager.skip('tutorial');
 
     // Modify the stager to skip one stage.
     // stager.skip('quiz');
     // stager.skip('practice');
     // stager.skip('practice-end');
-    // stager.skip('tour-end');
+    // stager.skip('tutorial-end');
     // stager.skip('test');
 
     return stager.getState();
